0856321e4ae9ae3780de2bf0c6f54746e68a94e4
Trần Văn Hoàng <93042377+chamuonfa@users.noreply.github.com>
2022-06-22 22:02:44 +0700

    commit 22h02!

--- src/components/page-flow1/flow1.js
+++ src/components/page-flow1/flow1.js
@@ -12,17 +12,19 @@ class Flow1 extends React.Component {
         return(
             <div className="flow1">
                 <Nav />
-                <div className="grid wide1 flow1-contents">
+                <div className="grid wide">
                     <div className="header-flow1">
-                        <div className="header-flow1 name">
-                            <div className="header-flow1 name id">#4859340543695496594</div>
-                            <div className="header-flow1 name compname">Cty TNHH MTV Mặt trời đỏ</div>
-                            {/* <Link to='/'>
-                                <div className="header-flow1 button">
-                                    <div className="buttontext"> Duyệt đơn </div>
-                                </div>
-                            </Link> */}
+                        <div className="header-flow1__name">
+                            <div className="header-flow1__name-info">
+                                <span className="header-flow1__name-id">#4859340543695496594</span>
+                                <span className="header-flow1__name-compname">Cty TNHH MTV Mặt trời đỏ</span>
+                                <span>Chờ duyệt</span>
+                                <span>Chưa duyệt</span>
+                            </div>
                         </div>
+                            <Link to='/'>
+                                <button className="header-flow1__button"> Duyệt đơn </button>
+                            </Link>
                     </div>
                     
                     <div className="contents-flow1">
